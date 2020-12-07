ee998995d83fddf0290dc0adaa4d345e1d81e8c0
Ula Kalinowska <ursz.kalinowska@gmail.com>
2020-12-07 23:45:23 +0100

    deployment to ropsten testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,25 @@
 const UlaToken = artifacts.require("UlaToken");
 const UlaTokenSale = artifacts.require("UlaTokenSale");
+let ulaToken
+let ulaTokenSale
 
-module.exports = async function(deployer) {
-  deployer.deploy(UlaToken, '1000000');
-  const ulaToken = await UlaToken.deployed()
+module.exports = function(deployer) {
+  deployer.deploy(UlaToken, 1000000).then(function(instance) {
+    ulaToken = instance;
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(UlaTokenSale, UlaToken.address, tokenPrice);
+  })/*.then(function(instance) {
+    ulaTokenSale = instance;
+    return ulaToken.transfer(ulaTokenSale.address, 500000)
+  })*/
+  //deployer.deploy(UlaToken, '1000000');
+  //const ulaToken = await UlaToken.deployed()
   
-  var tokenPrice = 1000000000000000;
-  deployer.deploy(UlaTokenSale, ulaToken.address, tokenPrice)
+  //var tokenPrice = 1000000000000000;
+  //deployer.deploy(UlaTokenSale, ulaToken.address, tokenPrice)
+  //const ulaTokenSale = await UlaTokenSale.deployed()
+  //await ulaToken.transfer(ulaTokenSale.address, 750000, {gas: 500000})
+  //var balance = await ulaToken.balanceOf(ulaTokenSale.address)
+  //console.log(balance.toNumber())
 };
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 App = {
     web3Provider: null,
     contracts: {},
+    account: '',
+    loading: false,
+    tokenPrice: '',
+    tokensSold: '',
+    tokensAvailable: '750000',
 
     init: function() {
         console.log("app initialized")
@@ -25,17 +30,94 @@ App = {
             App.contracts.UlaTokenSale.setProvider(App.web3Provider)
             App.contracts.UlaTokenSale.deployed().then(function(ulaTokenSale){
                 console.log("Ula Token Sale Address: ", ulaTokenSale.address)
-            }).done(function() {
+            });
+        }).done(function() {
                 $.getJSON("UlaToken.json", function(ulaToken) {
                     App.contracts.UlaToken = TruffleContract(ulaToken)
                     App.contracts.UlaToken.setProvider(App.web3Provider)
                     App.contracts.UlaToken.deployed().then(function(ulaToken){
                         console.log("Ula Token Address: ", ulaToken.address)
                     }); 
-            }); 
-        });
-    })
-}
+                    App.listenForEvents();
+                    return App.render();
+                }); 
+            })
+    },
+
+    listenForEvents: function() {
+        App.contracts.UlaTokenSale.deployed().then(function(instance) {
+            instance.Sell({}, {
+            fromBlock: 0,
+            toBlock: 'latest',
+        }).watch(function(err, event) {
+            console.log("event triggered: ", event)
+            App.render()
+        })
+        })
+    },
+
+    render: function() {
+        if(App.loading) {
+            return
+        };
+
+        App.loading = true;
+        var loader = $('#loader');
+        var content = $('#content');
+
+        loader.show();
+        content.hide();
+
+        web3.eth.getCoinbase(function(err, account){
+            if(err === null) {
+                console.log("account: ", account);
+                App.account = account;
+                $('#accountAddress').html("Your account: " + account)
+            }
+        })
+
+        App.contracts.UlaTokenSale.deployed().then(function(instance) {
+            ulaTokenSaleInstance = instance;
+            return ulaTokenSaleInstance.tokenPrice()
+        }).then(function(tokenPrice) {
+            App.tokenPrice = tokenPrice;
+            $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber())
+            return ulaTokenSaleInstance.tokensSold();
+        }).then(function(tokensSold) {
+            App.tokensSold = tokensSold.toNumber();
+            $('.tokens-sold').html(App.tokensSold)
+            $('.tokens-available').html(App.tokensAvailable)
+
+            var progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+            $('#progress').css('width', progressPercent + '%');
+
+            App.contracts.UlaToken.deployed().then(function(instance) {
+                ulaTokenInstance = instance;
+                return ulaTokenInstance.balanceOf(App.account)
+            }).then(function(balance) {
+                $('.dapp-balance').html(balance.toNumber())
+
+                App.loading = false;
+                loader.hide();
+                content.show();
+            })
+        }); 
+    },
+
+    buyTokens: function() {
+        $('#loader').show;
+        $('#content').hide;
+
+        var numberOfTokens = $('#numberOfTokens').val();
+
+        App.contracts.UlaTokenSale.deployed().then(function(instance) {
+            instance.buyTokens(numberOfTokens, {from: App.account, value: numberOfTokens * App.tokenPrice})
+        }).then(function(result) {
+            console.log("Tokens bought");
+            $('form').trigger('reset')
+        })
+    }
+        
 }
 
 $(function() {
